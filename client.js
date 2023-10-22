3b338d31733239511e6622108904d8b571604448
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-22 15:30:18 -0600

    put content for longest word method w/ no success yet

--- client.js
+++ client.js
@@ -11,3 +11,28 @@ const reversed = soccerGearItems.reverse();
 console.log('reversed', reversed);
 //render to html
 document.getElementById("reverse-a-string").innerHTML = soccerGearItems;
+
+//Find longest word in string
+const barcelona = ['Barcelona is the best city in Europe']
+
+function findLongestWordLength(barcelona) {
+  
+  let longestWord = '';
+ 
+  let words = barcelona.split('');
+  
+  for (let i = 0; i < words.length; i++) {
+    let individualWord = words[i];
+    
+    if (individualWord.length > longestWord.length ) {
+      longestWord = individualWord;
+    }
+  }
+
+  return longestWord.length;
+ 
+}  
+console.log(findLongestWordLength(barcelona));
+
+//render to html
+document.getElementById("longest-word").innerHTML = findLongestWordLength(fcBarcelona);
