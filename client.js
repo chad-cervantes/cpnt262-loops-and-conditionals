b3138fc656425a964cdbe150b9f2faa8bba20554
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-22 11:24:05 -0600

    made forEach content & rendered it to html file

---
+++ client.js
@@ -0,0 +1,6 @@
+const soccerGearItems = ['football boots', 'shin pads', 'track suit', 'soccer ball', 'pylons', 'air pump', 'water bottle']
+
+//forEach method
+soccerGearItems.forEach(soccerGearItem => {console.log(soccerGearItem)});
+//render to html
+document.getElementById("for-each").innerHTML = soccerGearItems;
